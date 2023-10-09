3bfe33196f2298a7af2e07657dc7614fa94bcdd5
Miguel <mbermudez@itpfp.com>
2023-10-09 11:29:04 +0200

    cambio

--- JS/index.js
+++ JS/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.sendFile((__dirname, '../cv.html'))
+  res.sendFile((__dirname, 'cv.html'))
 })
 
 
